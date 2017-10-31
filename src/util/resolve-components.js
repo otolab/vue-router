1ee47b56d4197043c4f5a16c395cd430b6306814
Naoto Kato <naoto.kato@plaid.co.jp>
2017-10-31 19:39:37 +0900

    fix for components: { [componentName]: (resolve) => resolve({..})}

--- src/util/resolve-components.js
+++ src/util/resolve-components.js
@@ -5,7 +5,7 @@ import { warn, isError } from './warn'
 
 export function resolveAsyncComponents (matched: Array<RouteRecord>): Function {
   return (to, from, next) => {
-    let hasAsync = false
+    const asyncRequests = []
     let pending = 0
     let error = null
 
@@ -16,56 +16,61 @@ export function resolveAsyncComponents (matched: Array<RouteRecord>): Function {
       // we want to halt the navigation until the incoming component has been
       // resolved.
       if (typeof def === 'function' && def.cid === undefined) {
-        hasAsync = true
-        pending++
+        asyncRequests.push({def, match, key})
+      }
+    })
 
-        const resolve = once(resolvedDef => {
-          if (isESModule(resolvedDef)) {
-            resolvedDef = resolvedDef.default
-          }
-          // save resolved on async factory in case it's used elsewhere
-          def.resolved = typeof resolvedDef === 'function'
-            ? resolvedDef
-            : _Vue.extend(resolvedDef)
-          match.components[key] = resolvedDef
-          pending--
-          if (pending <= 0) {
-            next()
-          }
-        })
+    if (!asyncRequests.length) next();
 
-        const reject = once(reason => {
-          const msg = `Failed to resolve async component ${key}: ${reason}`
-          process.env.NODE_ENV !== 'production' && warn(false, msg)
-          if (!error) {
-            error = isError(reason)
-              ? reason
-              : new Error(msg)
-            next(error)
-          }
-        })
+    pending = asyncRequests.length
 
-        let res
-        try {
-          res = def(resolve, reject)
-        } catch (e) {
-          reject(e)
+    asyncRequests.forEach((req) => {
+      const {def, match, key} = req;
+
+      const resolve = once(resolvedDef => {
+        if (isESModule(resolvedDef)) {
+          resolvedDef = resolvedDef.default
         }
-        if (res) {
-          if (typeof res.then === 'function') {
-            res.then(resolve, reject)
-          } else {
-            // new syntax in Vue 2.3
-            const comp = res.component
-            if (comp && typeof comp.then === 'function') {
-              comp.then(resolve, reject)
-            }
+        // save resolved on async factory in case it's used elsewhere
+        def.resolved = typeof resolvedDef === 'function'
+          ? resolvedDef
+          : _Vue.extend(resolvedDef)
+        match.components[key] = resolvedDef
+        pending--
+        if (pending <= 0) {
+          next()
+        }
+      })
+
+      const reject = once(reason => {
+        const msg = `Failed to resolve async component ${key}: ${reason}`
+        process.env.NODE_ENV !== 'production' && warn(false, msg)
+        if (!error) {
+          error = isError(reason)
+            ? reason
+            : new Error(msg)
+          next(error)
+        }
+      })
+
+      let res
+      try {
+        res = def(resolve, reject)
+      } catch (e) {
+        reject(e)
+      }
+      if (res) {
+        if (typeof res.then === 'function') {
+          res.then(resolve, reject)
+        } else {
+          // new syntax in Vue 2.3
+          const comp = res.component
+          if (comp && typeof comp.then === 'function') {
+            comp.then(resolve, reject)
           }
         }
       }
     })
-
-    if (!hasAsync) next()
   }
 }
 
